5badd0d8d6b5830f011564d1ba5d2f7e194be251
Alon Noga <54034642+alonnoga@users.noreply.github.com>
2024-12-23 16:31:47 +0200

    Update main.tf

--- mine/azure-storage/main.tf
+++ mine/azure-storage/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "example" {
 
 # Define the storage account
 resource "azurerm_storage_account" "example" {
-  name                     = "alonstorage${random_string.suffix.result}"
+  name                     = "alonstorage2${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
